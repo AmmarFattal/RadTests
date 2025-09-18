079c1aa1077b9b5abac81d0d5c787b65f0618e8d
AmmarFattal <57180330+AmmarFattal@users.noreply.github.com>
2025-09-18 20:52:08 +0300

    Adding files to repository

---
+++ Unit1.dfm
@@ -0,0 +1,79 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+  object Label1: TLabel
+    Left = 8
+    Top = 16
+    Width = 575
+    Height = 32
+    BiDiMode = bdLeftToRight
+    Caption = 'Press any of the buttons below to calculate the intrest'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -24
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ParentBiDiMode = False
+    ParentFont = False
+  end
+  object Label2: TLabel
+    Left = 264
+    Top = 167
+    Width = 13
+    Height = 32
+    Caption = '0'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -24
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ParentFont = False
+  end
+  object btnGold: TButton
+    Left = 8
+    Top = 72
+    Width = 94
+    Height = 40
+    Caption = 'Gold'
+    TabOrder = 0
+    OnClick = btnGoldClick
+  end
+  object btnSilver: TButton
+    Left = 248
+    Top = 72
+    Width = 94
+    Height = 40
+    Caption = 'Silver'
+    TabOrder = 1
+    OnClick = btnSilverClick
+  end
+  object btnStandard: TButton
+    Left = 508
+    Top = 72
+    Width = 94
+    Height = 40
+    Caption = 'Standard'
+    TabOrder = 2
+    OnClick = btnStandardClick
+  end
+  object ScrollBar1: TScrollBar
+    Left = 8
+    Top = 144
+    Width = 529
+    Height = 17
+    Min = -100
+    PageSize = 0
+    TabOrder = 3
+    OnChange = ScrollBar1Change
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,77 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    btnGold: TButton;
+    btnSilver: TButton;
+    btnStandard: TButton;
+    ScrollBar1: TScrollBar;
+    Label2: TLabel;
+    procedure ScrollBar1Change(Sender: TObject);
+    procedure btnGoldClick(Sender: TObject);
+    procedure btnSilverClick(Sender: TObject);
+    procedure btnStandardClick(Sender: TObject);
+  private
+    procedure CalculateInterest(bonus: integer);
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.btnSilverClick(Sender: TObject);
+begin
+  CalculateInterest(1);
+end;
+
+procedure TForm1.btnStandardClick(Sender: TObject);
+begin
+  CalculateInterest(0);
+end;
+
+procedure TForm1.CalculateInterest(bonus:integer);
+var
+  CurrentBalance:integer;
+  StandardRate, InterestRate:double;
+  msg:string;
+begin
+  StandardRate := 5;
+  StandardRate := (StandardRate + bonus)/100;
+  CurrentBalance := ScrollBar1.Position;
+  InterestRate := CurrentBalance*StandardRate;
+
+    if InterestRate > 0 then
+      msg := 'Interest will be added (Credited) to ur account : '
+    else
+      msg := 'Interest will be removed(Debited) from ur account : ';
+
+    Label1.Caption := msg + AnsiToUtf8(FloatToStrF(InterestRate,ffCurrency,2,2));
+
+
+
+end;
+
+procedure TForm1.btnGoldClick(Sender: TObject);
+begin
+  CalculateInterest(2);
+end;
+
+procedure TForm1.ScrollBar1Change(Sender: TObject);
+begin
+  Label2.Caption := IntToStr(ScrollBar1.Position);
+end;
+
+end.
